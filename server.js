109af5389ff9d2140cd348eb7e454f3118cc8daf
davesrose <dsr0116@yahoo.com>
2020-08-22 15:42:06 -0400

    updating mongodb for heroku

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const options = {
+  useMongoClient: true,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+};
+
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -18,10 +28,10 @@ mongoose.Promise = global.Promise;
 
 if(process.env.NODE_ENV == 'production'){
   // Gotten using `heroku config | grep MONGODB_URI` command in Command Line
-  mongoose.connect('mongodb://heroku_n87vsq8x:kisj52j6m9d4r34lvakphr682u@ds243335.mlab.com:43335/heroku_n87vsq8x');
+  mongoose.connect('mongodb://heroku_n87vsq8x:kisj52j6m9d4r34lvakphr682u@ds243335.mlab.com:43335/heroku_n87vsq8x', options);
 }
 else{
-  mongoose.createConnection('mongodb://localhost/policeDB');
+  mongoose.connect('mongodb://localhost/policeDB', options).catch(error => handleError(error));
 }
 // Start the API server
 app.listen(PORT, function() {
